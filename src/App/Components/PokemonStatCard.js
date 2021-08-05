cdcf161411c9f2c065a0451445202dfbd6502dd0
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-05 10:32:58 +0200

    Add transtions and effect

--- src/App/Components/PokemonStatCard.js
+++ src/App/Components/PokemonStatCard.js
@@ -4,23 +4,48 @@ import '../../Styles/PokemonStatCard.scss';
 
 const PokemonStatCard = ({ pokemon }) => (
   <div className="wrapper">
-    <div className="inner">
-      <h1>id</h1>
-      <p>
+    <div className="inner info">
+      <h2>id</h2>
+      <h2>
         {pokemon.id}
-      </p>
-      <p>
-        {pokemon.english}
-      </p>
-      <p>
-        {pokemon.japanese}
-      </p>
-      <p>
-        {pokemon.thumbnail}
-      </p>
-      <img src={pokemon.hires} alt={pokemon.id} />
+      </h2>
+      <h2>
+        {pokemon.height}
+      </h2>
+      <h2>
+        {pokemon.type[0]}
+      </h2>
+      <h2>
+        {pokemon.type[0]}
+      </h2>
+      <h3>
+        {pokemon.description}
+      </h3>
+    </div>
+    <img src={pokemon.hires} alt={pokemon.id} />
+    <div className="inner2 stats">
+      <h1>Pokemon Stats</h1>
+      <h2>
+        {pokemon.HP}
+      </h2>
+      <h2>
+        {pokemon.Attack}
+      </h2>
+      <h2>
+        {pokemon.Defense}
+      </h2>
+      <h2>
+        {pokemon.SpAttack}
+      </h2>
+      <h2>
+        {pokemon.SpDefense}
+      </h2>
+      <h2>
+        {pokemon.Speed}
+      </h2>
     </div>
   </div>
+
 );
 
 export default PokemonStatCard;
