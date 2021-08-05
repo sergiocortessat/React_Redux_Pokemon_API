86388f394db44a1adeb1e2ee530d3a37bd879d48
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-05 12:29:16 +0200

    Add style to cards

--- src/App/Components/PokemonStatCard.js
+++ src/App/Components/PokemonStatCard.js
@@ -1,29 +1,46 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../Styles/PokemonStatCard.scss';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import ProgressBar from './ProgressBar';
 
-const PokemonStatCard = ({ pokemon }) => (
-  <div className="wrapper">
-    <div className="inner info">
-      <h2>
-        ID
-        {' '}
-        #
-        {pokemon.id}
+const PokemonStatCard = ({ pokemon }) => {
+  const [completed, setCompleted] = useState([]);
 
-      </h2>
-      <h2>
-        HEIGHT
-        {' '}
-        {pokemon.height}
-      </h2>
-      <h2>
-        WEIGHT
-        {' '}
-        {pokemon.weight}
-      </h2>
-      {pokemon.type && (
+  const pokemonStats = [
+    pokemon.HP,
+    pokemon.Attack,
+    pokemon.Defense,
+    pokemon.SpecialAttack,
+    pokemon.SpecialDefense,
+    pokemon.Speed,
+  ];
+  useEffect(() => {
+    setCompleted(([...pokemonStats]));
+    // setCompleted(String(pokemon.Attack));
+  }, []);
+  console.log(pokemonStats);
+  return (
+    <div className="wrapper">
+      <div className="inner info">
+        <h2>
+          ID
+          {' '}
+          #
+          {pokemon.id}
+
+        </h2>
+        <h2>
+          HEIGHT
+          {' '}
+          {pokemon.height}
+        </h2>
+        <h2>
+          WEIGHT
+          {' '}
+          {pokemon.weight}
+        </h2>
+        {pokemon.type && (
         <div>
           <h2>TYPE</h2>
           <h2>
@@ -33,15 +50,15 @@ const PokemonStatCard = ({ pokemon }) => (
             {pokemon.type[1]}
           </h2>
         </div>
-      )}
-      {pokemon.evolution && (
-      <h2>
-        NEXT-EVOLUTION
-        {' '}
-        {pokemon.evolution.next[0][1].toUpperCase()}
-      </h2>
-      )}
-      {pokemon.ability && (
+        )}
+        {pokemon.evolution && (
+        <h2>
+          NEXT-EVOLUTION
+          {' '}
+          {pokemon.evolution.next[0][1].toUpperCase()}
+        </h2>
+        )}
+        {pokemon.ability && (
         <div>
           <h2>ABILITY</h2>
           <h2>
@@ -51,38 +68,31 @@ const PokemonStatCard = ({ pokemon }) => (
             {pokemon.ability[1][0]}
           </h2>
         </div>
-      )}
-      <div className="description">
-        <h3>
-          pokemon.description
-        </h3>
+        )}
+        <div className="description">
+          <h3>
+            {pokemon.description}
+          </h3>
+        </div>
+      </div>
+      <div className="stats-image">
+        <img src={pokemon.hires} alt={pokemon.id} />
+      </div>
+      <div className="inner2 stats">
+        <h2>POKEMON STATS</h2>
+        <div>
+          {completed.map((stat) => (
+            <ProgressBar
+              key="yes"
+              bgcolor="black"
+              completed={stat}
+            />
+          ))}
+        </div>
       </div>
     </div>
-    <img src={pokemon.hires} alt={pokemon.id} />
-    <div className="inner2 stats">
-      <h1>Pokemon Stats</h1>
-      <h2>
-        {pokemon.HP}
-      </h2>
-      <h2>
-        {pokemon.Attack}
-      </h2>
-      <h2>
-        {pokemon.Defense}
-      </h2>
-      <h2>
-        {pokemon.SpAttack}
-      </h2>
-      <h2>
-        {pokemon.SpDefense}
-      </h2>
-      <h2>
-        {pokemon.Speed}
-      </h2>
-    </div>
-    <LinearProgress value={90} />
-  </div>
 
-);
+  );
+};
 
 export default PokemonStatCard;
