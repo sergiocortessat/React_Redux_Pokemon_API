6448299f4cff60f6e0aa134cd4215712acdd9bfa
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-05 13:03:55 +0200

    Add style for mobile screens

--- src/App/Components/PokemonStatCard.js
+++ src/App/Components/PokemonStatCard.js
@@ -8,18 +8,17 @@ const PokemonStatCard = ({ pokemon }) => {
   const [completed, setCompleted] = useState([]);
 
   const pokemonStats = [
-    pokemon.HP,
-    pokemon.Attack,
-    pokemon.Defense,
-    pokemon.SpecialAttack,
-    pokemon.SpecialDefense,
-    pokemon.Speed,
+    { name: 'HP', stat: pokemon.HP, color: '#00ff00' },
+    { name: 'ATTACK', stat: pokemon.Attack, color: '#ff0000' },
+    { name: 'DEFENSE', stat: pokemon.Defense, color: '#0000ff' },
+    { name: 'SPECIAL ATTACK', stat: pokemon.SpecialAttack, color: '#ffff00' },
+    { name: 'SPECIAL DEFENSE', stat: pokemon.SpecialDefense, color: '#00ffff' },
+    { name: 'SPEED', stat: pokemon.Speed, color: '#ff00ff' },
   ];
   useEffect(() => {
     setCompleted(([...pokemonStats]));
     // setCompleted(String(pokemon.Attack));
   }, []);
-  console.log(pokemonStats);
   return (
     <div className="wrapper">
       <div className="inner info">
@@ -55,21 +54,22 @@ const PokemonStatCard = ({ pokemon }) => {
         <h2>
           NEXT-EVOLUTION
           {' '}
-          {pokemon.evolution.next[0][1].toUpperCase()}
+          {pokemon.evolution.next ? pokemon.evolution.next[0][1].toUpperCase() : 'NO-EVOLUTION'}
         </h2>
         )}
         {pokemon.ability && (
         <div>
           <h2>ABILITY</h2>
           <h2>
-            {pokemon.ability[0][0]}
+            {pokemon.ability[0] ? pokemon.ability[0][0] : '--'}
           </h2>
           <h2>
-            {pokemon.ability[1][0]}
+            {pokemon.ability[1] ? pokemon.ability[1][0] : '--'}
           </h2>
         </div>
         )}
         <div className="description">
+          <h2>DESCRIPTION</h2>
           <h3>
             {pokemon.description}
           </h3>
@@ -82,11 +82,14 @@ const PokemonStatCard = ({ pokemon }) => {
         <h2>POKEMON STATS</h2>
         <div>
           {completed.map((stat) => (
-            <ProgressBar
-              key="yes"
-              bgcolor="black"
-              completed={stat}
-            />
+            <>
+              <h2>{stat.name}</h2>
+              <ProgressBar
+                key={stat.name}
+                bgcolor={stat.color}
+                completed={stat.stat}
+              />
+            </>
           ))}
         </div>
       </div>
