4e3044ea1bfe91ed2fd28f69eb6b3e02229dab8f
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-06 19:43:22 +0200

    Add style to pokemon stat

--- src/App/Components/ProgressBar.js
+++ src/App/Components/ProgressBar.js
@@ -30,7 +30,7 @@ const ProgressBar = (props) => {
   return (
     <div style={containerStyles}>
       <div style={fillerStyles}>
-        <span style={labelStyles}>{`${completed}`}</span>
+        <span style={labelStyles}>{completed === 0 ? `${completed}` : 0}</span>
       </div>
     </div>
   );
