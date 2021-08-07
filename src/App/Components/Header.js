5ce83d0eb3951841bc235864a4e05634796c0abc
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-07 07:31:03 +0200

    Add logo link to home

--- src/App/Components/Header.js
+++ src/App/Components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../Assets/logo.svg';
 import '../../Styles/Header.scss';
 
 const Header = () => (
-  <div className="searchContainer" data-testid="header">
-    <img src={logo} alt="none" />
-  </div>
+  <Link to="/">
+    <div className="searchContainer" data-testid="header">
+      <img src={logo} alt="none" />
+    </div>
+  </Link>
 );
 
 export default Header;
