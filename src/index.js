35e8b7f931ec5326f45400ea46b055fcf882cde2
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-08-07 04:38:53 +0200

    Add register PWA

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
-// import react routers
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,7 +21,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
